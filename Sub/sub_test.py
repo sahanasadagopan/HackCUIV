c7b4b8229d3c089307a6062b8bd02be4c874bbc2
sahanasadagopan <sahana.sadagopan@gmail.com>
2018-02-25 01:39:20 -1200

    Led feature

--- Sub/sub_test.py
+++ Sub/sub_test.py
@@ -4,6 +4,7 @@ from pubnub.pnconfiguration import PNConfiguration
 from pubnub.pubnub import PubNub
 from pubnub.callbacks import SubscribeCallback
 from pubnub.enums import PNStatusCategory
+import RPi.GPIO as GPIO 
 
 pnconf = PNConfiguration()
 pnconf.publish_key = "pub-c-0dee2dd3-f22e-4d01-b741-ebe89f355b68"
@@ -18,6 +19,39 @@ class MySubscribeCallback(SubscribeCallback):
 	def message(self, pubnub, message):
 		print (str(message.message))
 		print(str(message.channel))
+		data =str( message.message)
+		split_data = data.split("""u'""")
+		print(split_data)
+		byte = str(split_data);
+                #byte = split_data[1].split(":");
+                if byte.find("left_byte': 15, ")>0:
+                    GPIO.setmode(GPIO.BCM)
+                    LED = 13
+                    GPIO.setup(LED, GPIO.OUT)
+                    GPIO.output(LED,GPIO.HIGH)
+		elif byte.find("left_byte': 9, ")>0:
+		   GPIO.setmode(GPIO.BCM)
+                   LED = 19
+                   GPIO.setup(LED, GPIO.OUT)
+                   GPIO.output(LED,GPIO.HIGH)
+		elif byte.find("left_byte': 0, ")>0:
+		   GPIO.setmode(GPIO.BCM)
+                   LED = 26
+                   GPIO.setup(LED, GPIO.OUT)
+                   GPIO.output(LED,GPIO.HIGH)
+		else:
+		   GPIO.setmode(GPIO.BCM)
+                   LED1 = 19
+		   LED2 = 13
+		   LED3 = 26
+                   GPIO.setup(LED1, GPIO.OUT)
+                   GPIO.output(LED1,GPIO.LOW) 
+		   GPIO.setup(LED2, GPIO.OUT)
+                   GPIO.output(LED2,GPIO.LOW)
+                   GPIO.setup(LED3, GPIO.OUT)
+                   GPIO.output(LED3,GPIO.LOW)
+
+
 	def status(self, pubnub, status):
 		print(status)
 	def onResponse( data, channel ):
